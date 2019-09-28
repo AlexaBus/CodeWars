760adcb3365b8ba7019917777a7a5d334b6c618d
AlexBus <alexandru.busurca@gmail.com>
2019-09-28 15:42:00 +0300

    Found a stray in an array

--- src/com/busurca/Main.java
+++ src/com/busurca/Main.java
@@ -1,11 +1,42 @@
 package com.busurca;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        DetectPangram dp = new DetectPangram();
-        System.out.println(dp.check("u0op>!@jmn!#41235@!#$%@#$512>zvby1kl#@%!$^3efqr857acwxdg1235125><Lhist"));
+        FindStray fs = new FindStray();
+
+        long startTime = System.nanoTime();
+        System.out.println(fs.stray(new int[]{17, 17, 3, 17, 17, 17, 17}));
+        long endTime = System.nanoTime();
+
+        long durationMySolution = (endTime - startTime);
+        System.out.println(durationMySolution);
+
+        startTime = System.nanoTime();
+        System.out.println(fs.straySecond(new int[]{17, 17, 3, 17, 17, 17, 17}));
+        endTime = System.nanoTime();
+
+        long durationSortSolution = (endTime - startTime);
+        System.out.println(durationSortSolution);
+
+        startTime = System.nanoTime();
+        System.out.println(fs.strayThird(new int[]{17, 17, 3, 17, 17, 17, 17}));
+        endTime = System.nanoTime();
+
+        long durationThirdSolution = (endTime - startTime);
+        System.out.println(durationThirdSolution);
+
+        startTime = System.nanoTime();
+        System.out.println(fs.strayFourth(new int[]{17, 17, 3, 17, 17, 17, 17}));
+        endTime = System.nanoTime();
+
+        long durationFourthSolution = (endTime - startTime);
+        System.out.println(durationFourthSolution);
 
     }
 }
